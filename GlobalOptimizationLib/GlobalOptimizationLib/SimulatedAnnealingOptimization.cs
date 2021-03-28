47bccc55b821741af9f209ac19c14ff62a471b7c
Jingzhen Liu <jingzhen_liu1990@hotmail.com>
2021-03-28 14:24:19 +0100

    update SimulatedAnnealling algorithm

--- GlobalOptimizationLib/GlobalOptimizationLib/SimulatedAnnealingOptimization.cs
+++ GlobalOptimizationLib/GlobalOptimizationLib/SimulatedAnnealingOptimization.cs
@@ -49,14 +49,107 @@ namespace GlobalOptimizationLib
             //Algorithm starts
             var T = 0.0000;
             var mu = 0.0000;
+            var x0 = initialguess.Clone() as double[];
+            var x = initialguess.Clone() as double[];
+            var fx0 = minerror;
+            var fx = minerror;
             for (int m = 0; m < maximumiteration; m++)
             {
                 T = m / maximumiteration;
                 mu=Math.Pow(10,T*100);
+                for (int k = 0; k < numberofneighbours; k++)
+                {
+                    var dx = getdx(mu, x, upperbound, lowerbound, k);
+                    var x1 = arrayplus(x, dx);
+                    x1 = constrainx(x1, upperbound, lowerbound);
+                    var fx1 = objectfun(x1);
+                    var df = fx1 - fx;
+                    if (validateX(df, T, fx, k))
+                    {
+                        x = x1.Clone() as double[];
+                        fx = fx1;
+                    }
+
+                    if (fx1 < minerror)
+                    {
+                        x0 = x1;
+                        fx0 = fx1;
+                    }
+                }
+                if (Math.Abs(minerror - fx0) < tolerence)
+                {
+                    break;
+                }
+                else
+                {
+                    minerror = fx0;
+                }
+                
             }
+            return x0;
 
         }
 
+        public bool validateX(double df, double T, double oldf,int k)
+        {
+            var result = false;
+            if (df<0)
+            {
+                var rnd = new MersenneTwister(k + 1);
+                var p = rnd.NextDouble();
+                var p1 = Math.Exp(-T * df / (Math.Abs(oldf) + 0.000000000001) / tolerence);
+                if (p < p1)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+        public double[] constrainx(double[] x, double[] u, double[] l)
+        {
+            var newx = new double[x.Length];
+            for (int i = 0; i < newx.Length; i++)
+            {
+                if (x[i] < l[i])
+                {
+                    newx[i] = l[i];
+                }
+                if (x[i] > u[i])
+                {
+                    newx[i] = u[i];
+                }
+                if (x[i] >= l[i] && x[i] <= u[i])
+                {
+                    newx[i] = x[i];
+                }
+            }
+            return newx;
+        }
+        public double[] arrayplus(double[]x,double[]y)
+        {
+            var result = new double[x.Length];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = x[i] + y[i];
+            }
+            return result;
+        }
+
+        public double[] getdx(double mu,double[] x, double[] u, double[] l, int k)
+        {
+            var result = new double[x.Length];
+            var rnd = new MersenneTwister(k + 1);
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = (2 * rnd.NextDouble() - 1)*u[i]-l[i];
+            }
+
+            result = mu_inv(result, mu);
+            return result;
+        }
+        
+        
         public double[] mu_inv(double[]y,double mu)
         {
             var result = new double[y.Length];

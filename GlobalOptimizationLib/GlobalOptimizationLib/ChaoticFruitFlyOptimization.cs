d290d2ea6a57250269e7a6e66928d8ef9843357d
Liu, Jingzhen (Reigate) <jingzhen.liu@towerswatson.com>
2021-04-21 13:36:20 +0100

    Speed up ChaoticFruitFlyOptimization

--- GlobalOptimizationLib/GlobalOptimizationLib/ChaoticFruitFlyOptimization.cs
+++ GlobalOptimizationLib/GlobalOptimizationLib/ChaoticFruitFlyOptimization.cs
@@ -23,7 +23,7 @@ namespace GlobalOptimizationLib
         {
             //Calculate Lambda values
             //var lambdamax=10;
-            var lambdamin = 0.00001;
+            var lambdamin = 0.0000001;
             var u = -9999999999.99;
             var l = 9999999999.99;
             for (int i = 0; i < lowerbound.Length; i++)
@@ -45,15 +45,17 @@ namespace GlobalOptimizationLib
             var location= new double[upperbound.Length]; 
             best = 9999999999999999.99;
             var C00 = 0.19;
-            var C10 = 0.45;
+
             for (int i = 0; i < sizeofinitialguess; i++)
             {
                 C00 = 4 * C00 * (1 - C00);
+
                 var tempx = new double[lowerbound.Length];
                 for (int j = 0; j < lowerbound.Length; j++)
                 {
                     tempx[j] = C00 * (upperbound[j] - lowerbound[j]) + lowerbound[j];
                 }
+                
                 var newerror = objectfun(tempx);
 
                 if (newerror < best)
@@ -63,44 +65,50 @@ namespace GlobalOptimizationLib
                     xstar = location.Clone() as double[];
                 }
             }
-            for (int i = 0; i < numofflies; i++)
+            for(int i=0;i<numofflies; i++)
             {
-                C10 = 4 * C10 * (1 - C10) ;
-                var tempx = new double[lowerbound.Length];              
-                for (int j = 0; j < lowerbound.Length; j++)
-                {
-                    tempx[j] = C10 * (upperbound[j]-lowerbound[j])+lowerbound[j];      
-                }
-                X.Add(i,tempx.Clone() as double[]);
-            }
+                X.Add(i, xstar.Clone() as double[]);  
+            };
 
             //Main loop
-            var bestsmell = best;
+            var bestsmell = new double[numofflies];
             var oldbest = best;
             var templambda=0.0;
-            var C30 = 0.68;
+            var C10 = 0.37;
+            var C20 = 0.79;
+            var C10Array = new double[maximumiteration,numofflies];
+            var C20Array = new double[maximumiteration, numofflies];
             for (int i = 0; i < maximumiteration; i++)
             {
-                templambda=lambdamax*Math.Exp(Math.Log(lambdamin/lambdamax)*i/maximumiteration);
                 for (int j = 0; j < numofflies; j++)
                 {
-                    var d = GenerateInteger(i + j + 1, lowerbound.Length);
-                    C30 = 4 * C30 * (1 - C30);
-                    var r1 = C30 * 2 - 1;
-
-                    X[j][d] = Math.Max(Math.Min(location[d] + templambda * r1, upperbound[d]), lowerbound[d]);
-
-                    bestsmell = objectfun(X[j]);
-                    if (bestsmell<best)
-                    {
-                        best = bestsmell;
-                        location = X[j].Clone() as double[];
-                        xstar =location.Clone() as double[];
-                        
-                    }
-                   
+                    C10 = 4 * C10 * (1 - C10);
+                    C20 = 4 * C20 * (1 - C20);
+                    C10Array[i, j] = C10;
+                    C20Array[i, j] = C20;
                 }
+            }
 
+            for (int i = 0; i < maximumiteration; i++)
+            {
+                templambda =lambdamax*Math.Exp(Math.Log(lambdamin/lambdamax)*i/maximumiteration);
+                Parallel.For(0,numofflies, j=>
+                {
+                    var d = (int)Math.Floor(C20Array[i,j]*lowerbound.Length);
+
+                    X[j][d] = Math.Max(Math.Min(location[d] + templambda * (C10Array[i, j]*2-1), upperbound[d]), lowerbound[d]);
+
+                    bestsmell[j] = objectfun(X[j]);
+                   
+                });
+                var minIndex = Array.IndexOf(bestsmell, bestsmell.Min());
+                if (bestsmell[minIndex] < best)
+                {
+                    best = bestsmell[minIndex];
+                    location = X[minIndex].Clone() as double[];
+                    xstar = location.Clone() as double[];
+
+                }
 
                 if (Math.Abs(oldbest - best) <= tolerance && i > Math.Floor((double)maximumiteration /3*2)) 
                 {
@@ -115,12 +123,6 @@ namespace GlobalOptimizationLib
             }
             
             return xstar;
-        }
-
-        public int GenerateInteger(int seed,int numofpara)
-        {
-            var rnd = new MersenneTwister(seed, true);
-            return rnd.Next(0,numofpara);
         }
        
     }
